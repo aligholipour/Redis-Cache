2520da2b952908f4f231eb18083022cf7ed9816e
Ali Gholipour <aligholipourkhalili@gmail.com>
2022-11-30 23:48:03 +0330

    Added the CreateProduct action method and then set the data to Redis

--- CRUD/SampleCrud/Controllers/HomeController.cs
+++ CRUD/SampleCrud/Controllers/HomeController.cs
@@ -1,21 +1,50 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
 using SampleCrud.Models;
 using System.Diagnostics;
+using System.Text.Json;
 
 namespace SampleCrud.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IDistributedCache _cache;
+        public HomeController(ILogger<HomeController> logger, IDistributedCache cache)
         {
             _logger = logger;
+            _cache = cache;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> CreateProduct()
+        {
+            var options = new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromHours(1),
+                SlidingExpiration = TimeSpan.FromMinutes(10),
+            };
+
+            var product = new ProductModel { Id = 1, Name = "Laptop", Price = 100, Quantity = 1 };
+
+            await _cache.SetStringAsync("Product_" + product.Id, JsonSerializer.Serialize(product), options);
+
+            return Ok();
+        }
+
+        public async Task<IActionResult> GetProduct()
+        {
+            var productId = 1;
+
+            var result = await _cache.GetStringAsync("Product_" + productId);
+
+            var product = JsonSerializer.Deserialize<ProductModel>(result);
+
+            return Ok();
         }
 
         public IActionResult Privacy()
